de9eec96a31a1c49d0ef1a394af85c53d6ca471f
Bo Geum Kim <epdlsemgks872@gmail.com>
2020-07-09 13:40:29 +0900

    python

---
+++ boj/boj_10870.py
@@ -0,0 +1,18 @@
+# Bronze 2
+
+import sys
+
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1 :
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+
+num = int(sys.stdin.readline().rstrip())
+
+print(fibo(num))
