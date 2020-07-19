58fd0d3ffa74452078f879e219504c5ad8c678ba
Bo Geum Kim <epdlsemgks872@gmail.com>
2020-07-19 14:28:21 +0900

    괄호 끼워넣기

--- boj/boj_11899.py
+++ boj/boj_11899.py
@@ -6,11 +6,15 @@ cnt = 0
 
 s = list(sys.stdin.readline().rstrip())
 
-for i in range(len(s)-1, 1, -1):
+top = s[::-1]
+
+for i in range(len(s)-1, -1, -1):
+    if top == "(" and s:
+        cnt += 1
+    elif top == ")":
+        if top == s[::-1]:
+            cnt += 1
+
     top = s.pop()
 
-    if 
-
 print(cnt)
-
-
