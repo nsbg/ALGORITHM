41ca585960c82890be9e469d8d57a3d1faf775e4
Bo Geum Kim <epdlsemgks872@gmail.com>
2020-07-19 14:25:49 +0900

    pizza voting

--- boj/boj_10280.py
+++ boj/boj_10280.py
@@ -4,20 +4,7 @@ import sys
 
 n, p = map(int, sys.stdin.readline().split())
 
-pizza = []
-
-for i in range(n):
-    pizzaList = list(map(str, sys.stdin.readline().split()))
-    pizza.append(int(pizzaList[0]))
-
-myPizza = pizza[p-1]
-
-pizza.remove(min(pizza))
-pizza.remove(max(pizza))
-
-avg = sum(pizza) / len(pizza)
-
-if myPizza <= avg:
-    print("YES")
-else:
+if n == 1 or n == p:
     print("NO")
+else:
+    
