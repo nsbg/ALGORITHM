2c75d95f4f03342f4143f8abfb433b0977db86cc
Bo Geum Kim <epdlsemgks872@gmail.com>
2020-08-01 21:48:37 +0900

    가운데 글자 가져오기

---
+++ programmers/pg_005.py
@@ -0,0 +1,15 @@
+# Level 1
+
+def solution(s):
+    answer = ''
+
+    if len(s) % 2 != 0:
+        i = len(s) // 2
+        answer += s[i]
+    else:
+        i = len(s)// 2 - 1
+        answer += s[i:i+2]
+
+    return answer
+
+k = print(solution(input()))
