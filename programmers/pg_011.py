b3ec1671972f20165943e0fffbe8e6122d5bebd3
Bo Geum Kim <epdlsemgks872@gmail.com>
2020-09-02 23:28:17 +0900

    예산(연습문제)

--- programmers/pg_011.py
+++ programmers/pg_011.py
@@ -1,7 +1,16 @@
 # 연습문제
 
-import sys
-
 def solution(d, budget):
     answer = 0
+    res = 0
+
+    d.sort()
+
+    for i in d:
+        if res + i <= budget:
+            res += i
+            answer += 1
+        else:
+            break
+
     return answer
