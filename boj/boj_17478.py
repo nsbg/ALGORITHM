0f0f4c0cf09f3ca866fe2ac696159ba02f228b83
Bo Geum Kim <epdlsemgks872@gmail.com>
2020-09-17 23:50:57 +0900

    풀어야 함

--- boj/boj_17478.py
+++ boj/boj_17478.py
@@ -1,10 +1,10 @@
 # Silver 5
 
-import sys
-
-
 def recursion(num):
-    
+    if num == 1:
+        print("\"재귀함수가 뭔가요?\"")
 
+n = int(input())
 
-start = "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다."
+print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
+recursion(n)
