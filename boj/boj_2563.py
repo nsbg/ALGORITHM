284b1e8348b4f8f782bea9789e18acb6fc632dd1
Bo Geum Kim <epdlsemgks872@gmail.com>
2020-09-27 16:31:32 +0900

    Add 색종이

---
+++ boj/boj_2563.py
@@ -0,0 +1,22 @@
+# Silver 5
+
+import sys
+
+n = int(input())
+
+square = [[0 for col in range(100)] for row in range(100)]
+cnt = 0
+
+for _ in range(n):
+    c, r = map(int, sys.stdin.readline().split())
+
+    for j in range(c, c+10):
+        for k in range(r, r+10):
+            square[j][k] = 1
+
+for s in square:
+    for q in s:
+        if q == 1:
+            cnt += 1
+
+print(cnt)
