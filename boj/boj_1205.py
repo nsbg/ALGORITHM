5d90546344f50ad8504c7c19fe45994e01bd81a1
Bo Geum Kim <epdlsemgks872@gmail.com>
2020-11-01 00:43:58 +0900

    Add 등수 구하기

---
+++ boj/boj_1205.py
@@ -0,0 +1,20 @@
+# Silver 4
+
+import sys
+
+# p : 랭킹 리스트에 올라갈 수 있는 점수 개수
+n, newScore, p = map(int, sys.stdin.readline().split())
+
+rank = list(map(int, sys.stdin.readline().split()))
+
+rank.append(newScore)
+
+rank = sorted(rank, reverse=True)
+
+# 새 점수가 랭킹 리스트 벗어나거나 랭킹 리스트가 꽉 찼는데 마지막 점수랑 같을 때
+if rank.index(newScore) > p or (n == p and rank[-1] == newScore):
+    print("-1")
+# 등수를 매길 수 있을 때
+else:
+    print(rank.index(newScore)+1)
+
