a427829666c3744c2ef5a16987c15308cd28bd1c
Bo Geum Kim <epdlsemgks872@gmail.com>
2021-01-24 17:30:25 +0900

    Add 그룹 단어 체커

--- boj/boj_1316.py
+++ boj/boj_1316.py
@@ -1,12 +1,20 @@
 # Silver 5
 
-import sys
-
 n = int(input())
 
 cnt = 0
 
 for _ in range(n):
-    word = input()
+    word = input().lower()
 
     for i in range(len(word)):
+        if i != len(word)-1:
+            if word[i] != word[i+1]:
+                if word[i] in word[i+2:]:
+                    break
+            else:
+                pass
+        else:
+            cnt += 1
+
+print(cnt)
