f190220935dfe92c81a54301d5be8634fca35dc8
Bo Geum Kim <epdlsemgks872@gmail.com>
2021-02-08 03:06:54 +0900

    콜라츠 추측

---
+++ programmers/pg_040.py
@@ -0,0 +1,15 @@
+def solution(num):
+    answer = 0
+
+    while num != 1:
+        if num % 2 != 0:
+            num = num * 3 + 1
+            answer += 1
+        else:
+            num //= 2
+            answer += 1
+
+    if answer > 500:
+        answer = -1
+
+    return answer
