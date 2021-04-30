05b57c01c273c7a8776d0db8dd5fe1c2d4a3b9a4
Bo Geum Kim <epdlsemgks872@gmail.com>
2021-04-30 21:48:19 +0900

    I AM IRONMAN

--- boj/boj_17264.py
+++ boj/boj_17264.py
@@ -1,10 +1,41 @@
-n, p = map(int, input().split())
+# Silver 3
 
+import sys
+
+n, p = map(int, input().split())
 w, l, g = map(int, input().split())
 
-playerList = set()
+findedPlayer = dict()
+
+score = 0
+win = False
 
 playerScore = []
 
 for _ in range(p):
-    playerScore.append(
+    name, wl = map(str, sys.stdin.readline().split())
+    findedPlayer[name] = wl
+
+for _ in range(n):
+    playerScore.append(sys.stdin.readline().rstrip())
+
+for p in playerScore:
+    if p not in findedPlayer.keys():
+        score -= l
+    else:
+        if findedPlayer[p] == "W":
+            score += w
+        elif findedPlayer[p] == "L":
+            score -= l
+
+    if score < 0:
+        score = 0
+
+    if score >= g:
+        win = True
+        break
+
+if not win:
+    print("I AM IRONMAN!!")
+else:
+    print("I AM NOT IRONMAN!!")
