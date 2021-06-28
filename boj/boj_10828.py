a5226ce5526aefd636444b258ff1b03188822553
nsbg <epdlsemgks872@gmail.com>
2021-06-29 05:15:01 +0900

    스택

---
+++ boj/boj_10828.py
@@ -0,0 +1,29 @@
+# Silver 4
+
+import sys
+
+n = int(input())
+stack = []
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0] == "push":
+        stack.append(int(command[1]))
+    elif command[0] == "pop":
+        if not stack:
+            print("-1")
+        else:
+            print(stack.pop(-1))
+    elif command[0] == "size":
+        print(len(stack))
+    elif command[0] == "empty":
+        if not stack:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == "top":
+        if not stack:
+            print("-1")
+        else:
+            print(stack[-1])
