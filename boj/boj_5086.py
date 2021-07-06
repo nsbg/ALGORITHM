71ce7c1d8375a854db3f82760ea8ff717b7c29fc
nsbg <epdlsemgks872@gmail.com>
2021-07-07 00:34:04 +0900

    Factors And Multiples

---
+++ boj/boj_5086.py
@@ -0,0 +1,14 @@
+# Bronze 3
+
+while True:
+    a, b = map(int, input().split())
+
+    if a+b == 0:
+        break
+    
+    if a%b == 0:
+        print("multiple")
+    elif b%a == 0:
+        print("factor")
+    else:
+        print("neither")
