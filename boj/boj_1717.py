fc92a34fd9fc96e63822a0d0c8459ae2b99fe3a9
unknown <epdlsemgks872@gmail.com>
2021-08-19 18:32:26 +0900

    집합의 표현

---
+++ boj/boj_1717.py
@@ -0,0 +1,40 @@
+# Gold 4
+
+import sys
+
+sys.setrecursionlimit(10**8)
+
+n, m = map(int, input().split())
+
+# 각 노드의 초기 부모 노드는 자기 자신
+parent = [i for i in range(n+1)]
+
+def union(x, y):
+    x = parent[x]
+    y = parent[y]
+
+    # 더 작은 노드가 부모
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+        
+def find(currentNode):
+    if parent[currentNode] == currentNode:
+        return currentNode
+    else:
+        parent[currentNode] = find(parent[currentNode])
+        
+        return parent[currentNode]
+
+for _ in range(m):
+    check, a, b = map(int, input().split())
+
+    if check:
+        # 부모 노드 같은지 확인
+        if find(a) == find(b):
+            print("YES")
+        else:
+            print("NO")
+    else:
+        union(a, b)
