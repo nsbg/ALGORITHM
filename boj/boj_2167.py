a8b06fad5704b780bd9eb52e8f336118c7c5c17b
nsbg <epdlsemgks872@gmail.com>
2021-09-26 19:50:18 +0900

    2차원 배열의 합(DP)

--- boj/boj_2167.py
+++ boj/boj_2167.py
@@ -15,18 +15,8 @@ dp = [[0]*(m+1) for _ in range(n+1)]
 for i in range(1, n+1):
     for j in range(1, m+1):
         dp[i][j] = dp[i][j-1]+numList[i-1][j-1]+dp[i-1][j]-dp[i-1][j-1]
-print(dp)
 
-# for _ in range(k):
-#     i, j, x, y = map(int, sys.stdin.readline().split())
+for _ in range(k):
+    i, j, x, y = map(int, sys.stdin.readline().split())
 
-#     result = 0
-    
-#     if i == x and j == y:
-#         result = numList[i-1][j-1]
-#     else:
-#         for row in range(i-1, x):
-#             for col in range(j-1, y):
-#                 result += numList[row][col]
-
-#     print(result)
+    print(dp[x][y]-dp[x][j-1]-dp[i-1][y]+dp[i-1])
