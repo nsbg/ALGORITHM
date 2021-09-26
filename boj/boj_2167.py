30ff4d86a01a210a394d4ef58c5408cd50918854
nsbg <epdlsemgks872@gmail.com>
2021-09-26 19:49:28 +0900

    2차원 배열의 합

--- boj/boj_2167.py
+++ boj/boj_2167.py
@@ -1,8 +1,32 @@
-# n : 세로 줄, m : 가로 줄
-n, m = map(int, input().split())
+# Bronze 1
 
-numList = [list(map(int, input().split())) for _ in range(n)]
+import sys
+
+# n : 세로 줄, m : 가로 줄
+n, m = map(int, sys.stdin.readline().split())
+
+numList = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
 k = int(input())
 
-for _ in range(k):
+# 배열 인덱스 1부터 시작하려고 n, m에 1씩 더한 크기로 생성
+dp = [[0]*(m+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = dp[i][j-1]+numList[i-1][j-1]+dp[i-1][j]-dp[i-1][j-1]
+print(dp)
+
+# for _ in range(k):
+#     i, j, x, y = map(int, sys.stdin.readline().split())
+
+#     result = 0
+    
+#     if i == x and j == y:
+#         result = numList[i-1][j-1]
+#     else:
+#         for row in range(i-1, x):
+#             for col in range(j-1, y):
+#                 result += numList[row][col]
+
+#     print(result)
