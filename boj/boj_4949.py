6b88e9af900ed5534508933be53a12b2d54085b9
nsbg <epdlsemgks872@gmail.com>
2021-10-16 22:55:38 +0900

    The Balance of the World

---
+++ boj/boj_4949.py
@@ -0,0 +1,26 @@
+# Silver 4
+
+import sys
+
+while True:
+    words = sys.stdin.readline().rstrip()
+
+    if words == ".":
+        break
+
+    stack = []
+
+    for w in words:
+        if w == "(" or w == "[":
+            stack.append(w)
+        elif w == ")":
+            if len(stack) != 0 and stack[-1] == "(":
+                stack.pop()
+        elif w == "]":
+            if len(stack) != 0 and stack[-1] == "[":
+                stack.pop()
+    
+    if len(stack) !=0 :
+        print("no")
+    else:
+        print("yes")
