9ec934f2157691868f3e271b901169238be74c54
Bo geum Kim <epdlsemgks872@gmail.com>
2022-02-02 23:34:29 +0900

    Jelly Bean

---
+++ boj/boj_11256.py
@@ -0,0 +1,29 @@
+# Silver 5
+
+import sys
+
+T = int(input())
+
+for _ in range(T):
+    J, N = map(int, sys.stdin.readline().split())
+
+    cnt = 0
+    tmp = 0
+
+    mul = []
+    
+    for _ in range(N):
+        R, C = map(int, sys.stdin.readline().split())
+        
+        mul.append(R*C)
+    
+    print(sorted(mul, reverse=True))
+
+    for m in sorted(mul, reverse=True):
+        J -= m
+        cnt += 1
+
+        if J <= 0:
+            break
+
+    print(cnt)
