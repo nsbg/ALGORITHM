cfcc275d9ec3ab3c65564104ccdc65f201dd8eef
Bo geum Kim <epdlsemgks872@gmail.com>
2022-02-03 13:51:46 +0900

    Pizza Voting

--- boj/boj_10280.py
+++ boj/boj_10280.py
@@ -1,10 +1,16 @@
-# Bronze 3
+# Bronze 1
 
 import sys
 
 n, p = map(int, sys.stdin.readline().split())
 
-if n == 1 or n == p:
-    print("NO")
+for i in range(n):
+    c, w = sys.stdin.readline().split()
+
+x = (n-1)//3+1
+y = x+(n-p)//3
+
+if x <= p <= y:
+    print("YES")
 else:
-    
+    print("NO")
