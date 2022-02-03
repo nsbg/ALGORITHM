3015c3334d6aca637053c0b753a6f88c4030c376
Bo geum Kim <epdlsemgks872@gmail.com>
2022-02-03 13:54:51 +0900

    Pizza Voting(수정)

--- boj/boj_10280.py
+++ boj/boj_10280.py
@@ -7,8 +7,8 @@ n, p = map(int, sys.stdin.readline().split())
 for i in range(n):
     c, w = sys.stdin.readline().split()
 
-x = (n-1)//3+1
-y = x+(n-p)//3
+x = n//3+1
+y = x+(n-1)//3
 
 if x <= p <= y:
     print("YES")
