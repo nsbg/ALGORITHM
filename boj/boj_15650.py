7d21bdf0cc3f65e86bf83c7fe44e2a0d65264fab
Bo geum Kim <epdlsemgks872@gmail.com>
2022-02-04 13:00:33 +0900

    N과 M(2)

---
+++ boj/boj_15650.py
@@ -0,0 +1,21 @@
+# Silver 3
+# 15649번과 차이점 : 수열은 오름차순이어야 함
+ 
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+arr = []
+
+def DFS(j):
+    if len(arr) == M:
+        print(*arr, sep=' ')
+        return
+
+    for i in range(j, N+1):
+        if i not in arr:
+            arr.append(i)
+            DFS(j+1)
+            arr.pop()
+    
+DFS(1)
