9aa1e66ef7b3064c09569c103a25d0410d113d8f
Bo geum Kim <epdlsemgks872@gmail.com>
2022-03-23 00:20:36 +0900

    부녀회장이 될테야

---
+++ boj/boj_2775.py
@@ -0,0 +1,18 @@
+# Bronze 1
+
+import sys
+
+T = int(input())
+
+for _ in range(T):
+    floor = int(input())
+    room = int(input())
+
+    person = [i for i in range(1, room+1)]
+
+    for _ in range(floor):
+        for idx in range(1, room):
+            person[idx] += person[idx-1]
+            print(person)     
+
+    print(person[-1])
