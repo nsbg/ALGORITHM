79a6e31800c87d9803d9145bd5840336831c2207
Bo geum Kim <epdlsemgks872@gmail.com>
2022-03-23 08:22:45 +0900

    팰린드롬인지 확인하기

---
+++ boj/boj_10988.py
@@ -0,0 +1,19 @@
+# Bronze 1
+
+import sys
+
+S = list(input())
+
+stack = []
+
+for s in S:
+    stack.append(s)
+
+print(S)
+
+while len(S) > 0:
+    if S.pop() != stack.pop(0):
+        print(0)
+        sys.exit(0)
+
+print(1)
