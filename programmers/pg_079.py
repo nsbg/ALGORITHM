b8896dc5fc3e901aa36c64cae705d0ed38a089ca
Bo geum Kim <epdlsemgks872@gmail.com>
2023-02-18 22:35:31 +0900

    햄버거 만들기

---
+++ programmers/pg_079.py
@@ -0,0 +1,19 @@
+# Level 1
+
+def solution(ingredient):
+    answer = 0
+    
+    stack = []
+    
+    for i in ingredient:
+        stack.append(i)
+        
+        if stack[-4:] == [1, 2, 3, 1]:
+            answer += 1
+            
+            for _ in range(4):
+                stack.pop()
+                
+    return answer
+
+solution([2, 1, 1, 2, 3, 1, 2, 3, 1])
