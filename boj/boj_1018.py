d86c4b9d9129e8330312ced89325bec0963c776b
nsbg <epdlsemgks872@gmail.com>
2023-11-05 20:56:55 +0900

    체스판 만들기

--- boj/boj_1018.py
+++ boj/boj_1018.py
@@ -1,8 +1,32 @@
-# Silver 5
+# Silver 4
 
 import sys
 
-n, m = map(int, sys.stdin.readline().split())
+rows, cols = map(int, sys.stdin.readline().split())
 
+board = [sys.stdin.readline().rstrip() for _ in range(rows)]
 
-w
+result = []
+
+for row in range(rows-7):
+    for col in range(cols-7):
+        cnt1, cnt2 = 0, 0
+        
+        # 나눠진 정사각형별로 확인
+        for r in range(row, row+8):
+            for c in range(col, col+8):
+                if (r+c)%2 == 0:
+                    if board[r][c] != 'W':
+                        cnt1 += 1
+                    if board[r][c] != 'B':
+                        cnt2 += 1
+                else:
+                    if board[r][c] != 'B':
+                        cnt1 += 1
+                    if board[r][c] != 'W':
+                        cnt2 += 1
+                        
+        result.append(cnt1)
+        result.append(cnt2)
+
+print(min(result))
