979bed265386dfdb704af5e105868df84adf5be8
nsbg <epdlsemgks872@gmail.com>
2023-12-08 17:30:52 +0900

    최댓값

--- boj/boj_2566.py
+++ boj/boj_2566.py
@@ -5,18 +5,18 @@ board = []
 row = 0
 col = 0
 
-for i in range(9):
+for _ in range(9):
     num = list(map(int, input().split()))
     board.append(num)
     
 m = max(board[0])
 
-for j in range(1, len(board)):
-    if m < max(board[j]):
-        m = max(board[j])
+for i in range(0, len(board)):
+    if m <= max(board[i]):
+        m = max(board[i])
         
-        row = j
-        col = board[j].index(max(board[j]))
+        row = i
+        col = board[i].index(m)
         
 print(m)
 print(row+1, col+1)
