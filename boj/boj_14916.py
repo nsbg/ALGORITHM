c201505303064d72510642a94bc2e5fde00a2a37
Kim bo geum <53206051+nsbg@users.noreply.github.com>
2024-01-02 19:49:10 +0900

    거스름돈

--- boj/boj_14916.py
+++ boj/boj_14916.py
@@ -2,17 +2,34 @@
 
 import sys
 
-n = int(sys.stdin.readline().rstrip())
+N = int(sys.stdin.readline().strip())
 
-cnt = 0
+answer = 0
 
-if n < 0 or n == 1 or n == 3:
-    print("-1")
-
-while n > 0:
-    if n % 5 == 0:
-        print(cnt + n // 5)
-        break
+# 5보다 작은 경우
+if N<5:
+    if N%2 == 0:
+        answer += N//2
     else:
-        n -= 2
-        cnt += 1
+        print("-1")
+        exit()
+
+
+if N%5 == 0:                # 5의 배수인 경우
+    answer += N//5
+elif N>5 and (N%5)%2 == 0:  # 5보다 크고 5로 나눈 나머지가 짝수인 경우 ex) 12
+    answer += N//5
+
+    N -= (N//5)*5
+
+    answer += N//2
+elif N>5 and (N%5)%2 != 0:  # 5보다 크고 5로 나눈 나머지가 홀수인 경우 ex) 13
+    temp = N//5
+
+    answer += temp-1
+
+    N -= (temp-1)*5
+
+    answer += N//2
+
+print(answer)
